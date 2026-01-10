b2a526e9781825890acae44be9c316f07f8a0d2f
Dancying <dancying@foxmail.com>
2026-01-11 04:22:27 +0800

    fix: 修复 404 模板结构

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import redirect
+from flask import render_template
 from flask import request
 
 import settings
@@ -14,6 +15,9 @@ legado_api = Blueprint("legado_api", __name__, url_prefix=settings.API_PREFIX, t
 
 logger = get_logger()
 
+SEARCH_ROUTE = "/search"
+PROXY_ROUTE = "/proxy"
+
 website_scraper_mapping = {
     "sixnine": SixNineScraper(),
     "flysky": FlySkyScraper(),
@@ -27,7 +31,7 @@ def legado_api_index():
     return redirect("https://git.dancying.cn/Dancying/NovelService/src/master/README.md", code=301)
 
 
-@legado_api.route("/search", methods=["POST"])
+@legado_api.route(SEARCH_ROUTE, methods=["POST"])
 def legado_search():
     logger.info(f"Request from [{request.remote_addr}]: {request.form}")
     keyword = request.form.get("keyword")
@@ -36,7 +40,7 @@ def legado_search():
     return html_data
 
 
-@legado_api.route("/proxy", methods=["GET"])
+@legado_api.route(PROXY_ROUTE, methods=["GET"])
 def legado_proxy():
     logger.info(f"Request from [{request.remote_addr}]: {request.args}")
     url = request.args.get("url")
@@ -49,11 +53,14 @@ def legado_proxy():
 def legado_book_source():
     logger.info(f"Access from [{request.remote_addr}]: Returning BookSource.json")
     target_url = f"{settings.BASE_URL}{settings.API_PREFIX}"
-    return get_latest_source().replace("https://api.dancying.cn", target_url)
+    result = get_latest_source()
+    result = result.replace("https://api.dancying.cn/legado/search", f"{target_url}{SEARCH_ROUTE}")
+    result = result.replace("https://api.dancying.cn/legado/proxy", f"{target_url}{PROXY_ROUTE}")
+    return result
 
 
 @legado_api.app_errorhandler(404)
 def handle_404(e):
-    if request.path.startswith(settings.API_PREFIX):
-        logger.warning(f"404 Not Found within API: [{request.remote_addr}] {request.path}")
-        return render_template("404.html", path=request.path), 404
+    logger.warning(f"404 Not Found within API: [{request.remote_addr}] {request.path}")
+    site = request.form.get("site") or request.args.get("site")
+    return render_template("404.html", url=request.url, site=site), 404
